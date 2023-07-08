4ad29aadbfcfefaa83bfbc898e7ab65633461255
tomo <o_tomo_1990@yahoo.co.jp>
2023-07-08 23:06:51 +0900

    ToDoが一時的に登録できる

--- todo.js
+++ todo.js
@@ -6,12 +6,30 @@ class ToDos extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      todo: "",
       todos: [
         { id: 1, name: "本を買う" },
         { id: 2, name: "クリーニング屋に行く" },
         { id: 3, name: "床屋に行く" },
       ],
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ todo: event.target.value });
+  }
+
+  handleSubmit(event) {
+    const ids = this.state.todos.map((todo) => todo.id);
+    this.setState((state) => {
+      return {
+        todos: [...state.todos, { id: ids + 1, name: this.state.todo }],
+      };
+    });
+    this.setState({ todo: "" });
+    event.preventDefault();
   }
 
   render() {
@@ -20,6 +38,17 @@ class ToDos extends React.Component {
     });
     return (
       <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={this.state.todo}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="登録" />
+        </form>
         <ul>{todos}</ul>
       </div>
     );
